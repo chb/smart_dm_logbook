840b7d653f2f3b7e5aae9ecd2b8800324ab0d77c
Arjun Sanyal <arjun.sanyal@childrens.harvard.edu>
2013-03-11 14:21:47 -0400

    Add note to run from top level to get_auth_reqs

--- bin/get_authed_reqs.py
+++ bin/get_authed_reqs.py
@@ -9,13 +9,14 @@
 # The script finds the returned "external_id" i.e. the random request_id
 # sent with the auth request and updates that row with the person and record
 # ids.
+#
+# NOTE: must be called from the top-level directory!
 
 import os
 import sys
 base = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(base+'/../healthvault/healthvault')
 sys.path.append(base+'/..')
-print sys.path
 
 import healthvault
 import settings
@@ -24,6 +25,9 @@ import sqlite3
 DEBUG = True
 
 if __name__ == "__main__":
+
+    print "\n\nNOTE: call me from the top-level dir!\n\n"
+
     hv_conn = healthvault.HVConn()
     reqs = hv_conn.getAuthorizedConnectRequests()
     for req in reqs:
